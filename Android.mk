ea6eb42744ce8312b6bbfc0d79db453ce3c7b471
zzc <zzc@rock-chips.com>
2016-12-07 18:01:05 +0800

    add Android.mk for usb_modeswitch
    Change-Id: I6dee3ef8ad5d95c7bce69580488e64626b5bff98
    Signed-off-by: zzc <zzc@rock-chips.com>

---
+++ Android.mk
@@ -0,0 +1,12 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+LIBUSB_DIR :=external/libusb
+common_src := libusb/core.c
+common_include :=	$(LOCAL_PATH)/ \
+			$(LOCAL_PATH)/libusb \
+			$(LIBUSB_DIR)/libusb
+LOCAL_MODULE := libusb-compat
+LOCAL_SRC_FILES :=$(common_src)
+LOCAL_C_INCLUDES +=$(common_include)
+LOCAL_SHARED_LIBRARIES := libusb
+include $(BUILD_SHARED_LIBRARY)
